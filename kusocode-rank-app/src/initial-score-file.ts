229a990f6596e9317077dea0d7f1e4caa98d6f67
hmatsu47 <35142774+hmatsu47@users.noreply.github.com>
2022-02-13 23:39:39 +0900

    アップロードミスしたので一旦削除

--- kusocode-rank-app/src/initial-score-file.ts
+++
@@ -1,6 +0,0 @@
-export const scoreByTeam = [
-  { name: "チームA", score: 0 },
-  { name: "チームB", score: 0 },
-  { name: "チームC", score: 0 },
-  { name: "チームD", score: 0 },
-];
--- kusocode-rank-app/src/utils/apiHandler.ts
+++
@@ -1,6 +0,0 @@
-// APIからデータ取得
-export const getApiData = async (url: string) => {
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
-};
